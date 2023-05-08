a0ac6d2c559922ce14d76aa5c58770eff8715aa6
Ronnieaza <130249058+Ronnieaza@users.noreply.github.com>
2023-05-08 04:15:22 -0900

    Add files via upload
    this is my cmp 104 code

---
+++ account.py
@@ -0,0 +1,14 @@
+class Account:
+    def __init__(self):
+        self.name = "ANOZIE RONNIE" \
+                    ""
+        self.account_number = 2266345707
+        self.account_balance = 200000
+
+    def deposite(self, amount):
+        self.account_balance = self.account_balance + amount
+        print(self.account_balance)
+
+    def withdraw(self, amount):
+        self.account_balance = self.account_balance - amount
+        print(self.account_balance)
